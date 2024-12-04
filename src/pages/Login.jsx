35a5614dd6e114d502351ed9f6907cf9c0dca1e2
ACER <210220@softwarica.edu.np>
2024-12-04 23:10:08 +0545

    correction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         setPassword("");
 
         setTimeout(() => {
-            navigate('/Form');
+            navigate('/VerifyEmail');
         }, 2000);
     }
 
--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const VerifyEmail = () => {
 return (
-    <div>
+    <div  className='mainContainer bg-green-700 h-[100vh] w-full flex'>
+
+        <div className="LeftSec w-[50%] h-full bg-red-700">
+
+        </div>
+
+
+        <div className="rightSec w-[50%] h-full bg-green-800">
+            
+        </div>
 
     </div>
 )
